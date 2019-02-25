bd170d5ae3461e105658ddbde98c9f105ba0c857
mertkanyener <mertkanyener@outlook.com>
2019-02-25 22:03:10 +0300

    Oauth2 configured and can send access_tokens

--- src/main/java/com/mertkan/eventproject/config/WebSecurityConfig.java
+++ src/main/java/com/mertkan/eventproject/config/WebSecurityConfig.java
@@ -2,13 +2,6 @@ package com.mertkan.eventproject.config;
 
 import com.mertkan.eventproject.encryption.Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,24 +11,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.filter.CompositeFilter;
-
-import javax.servlet.Filter;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -48,10 +27,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    @Qualifier("oauth2ClientContext")
-//    OAuth2ClientContext oAuth2ClientContext;
 
     @Bean
     @Override
@@ -72,60 +47,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/").permitAll().and().csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }}
-//                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-
-//
-//    @Bean
-//    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-//        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
-//        registration.setFilter(filter);
-//        registration.setOrder(-100);
-//        return registration;
-//    }
-//
-//
-//    @Bean
-//    @ConfigurationProperties("facebook")
-//    public ClientResources facebook() {
-//        return new ClientResources();
-//    }
-//    private Filter ssoFilter() {
-//        CompositeFilter filter = new CompositeFilter();
-//        List<Filter> filters = new ArrayList<>();
-//        filters.add(ssoFilter(facebook(), "/login/facebook"));
-//        filter.setFilters(filters);
-//        return filter;
-//    }
-//
-//    private Filter ssoFilter(ClientResources client, String path) {
-//        OAuth2ClientAuthenticationProcessingFilter oAuth2ClientAuthenticationFilter = new OAuth2ClientAuthenticationProcessingFilter(path);
-//        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(client.getClient(), oAuth2ClientContext);
-//        oAuth2ClientAuthenticationFilter.setRestTemplate(oAuth2RestTemplate);
-//        UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(),
-//                client.getClient().getClientId());
-//        tokenServices.setRestTemplate(oAuth2RestTemplate);
-//        oAuth2ClientAuthenticationFilter.setTokenServices(tokenServices);
-//        return oAuth2ClientAuthenticationFilter;
-//    }
-//
-//}
-//
-//class ClientResources {
-//
-//    @NestedConfigurationProperty
-//    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
-//
-//    @NestedConfigurationProperty
-//    private ResourceServerProperties resource = new ResourceServerProperties();
-//
-//    public AuthorizationCodeResourceDetails getClient() {
-//        return client;
-//    }
-//
-//    public ResourceServerProperties getResource() {
-//        return resource;
-//    }
-//}
 
 
 
