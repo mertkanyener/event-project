76ce00cb2c614f366b1b5a7528e8559b4fb7c857
mertkanyener <mertkanyener@outlook.com>
2019-05-22 20:04:51 +0300

    fsdafasdf

--- src/main/java/com/mertkan/eventproject/config/WebSecurityConfig.java
+++ src/main/java/com/mertkan/eventproject/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/events**", "/artists**", "/artists/*/*",
+                    .antMatchers("/events**", "/artists/**",
                             "/venues**", "/facebook**",
                             "/oauth/token").permitAll()
 
