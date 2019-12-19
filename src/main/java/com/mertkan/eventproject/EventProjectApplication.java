b360ea56fbe921168dd94b2a18f3c3ff07aae747
myener <mertkanyener@outlook.com>
2019-12-19 21:59:30 +0300

    03.12.2019 - Tuesday

--- src/main/java/com/mertkan/eventproject/EventProjectApplication.java
+++ src/main/java/com/mertkan/eventproject/EventProjectApplication.java
@@ -4,10 +4,11 @@ import com.mertkan.eventproject.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableConfigurationProperties(FileStorageProperties.class)
 @SpringBootApplication
-public class EventProjectApplication {
+public class EventProjectApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
